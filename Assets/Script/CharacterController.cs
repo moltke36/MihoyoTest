2eb6fd07e3b0ce1e50e9844f73b4d2edd90e79ee
junhaofu <516503324@qq.com>
2020-03-06 16:31:34 -0700

    Import model

---
+++ Assets/Script/CharacterController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class CharacterController : MonoBehaviour
+{
+
+    public float speed;
+    public float MoveX, MoveZ;
+
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputTick();
+        Movement();
+    }
+
+    void InputTick()
+    {
+        MoveX = Input.GetAxis("Horizontal");
+        MoveZ = Input.GetAxis("Vertical");
+    }
+
+
+    void Movement()
+    {
+        float xDeltaPos = MoveX * speed * Time.deltaTime;
+        float zDeltaPos = MoveZ * speed * Time.deltaTime;
+
+        animator.SetFloat("MovementInput", Mathf.Max(Mathf.Abs(MoveX), Mathf.Abs(MoveZ)));
+        //print(animator.GetInteger("Direction"));
+
+        transform.Translate(new Vector3(xDeltaPos, 0, zDeltaPos));
+    }
+}

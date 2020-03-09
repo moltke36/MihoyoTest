0d6e0d51ccb9fba9920f2888ba0520e7fd015d4e
junhaofu <516503324@qq.com>
2020-03-09 16:33:47 -0600

    Update Character Orient

--- Assets/Script/CameraControl.cs
+++ Assets/Script/CameraControl.cs
@@ -4,15 +4,69 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
+    public float ControlRotationSensitivity = 3.0f;
+    public float RotationSpeed = 0.0f;
+    public Character character;
+    public Quaternion pivotTargetLocalRotation;
+    public Vector2 CameraInput;
+
+    public float PositionSmoothDamp;
+    public Vector3 cameraVelocity;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateControllerRotation();
+    }
+
+    public void FixedUpdate()
+    {
+        CameraInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+        if (character)
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, character.transform.position, ref cameraVelocity, PositionSmoothDamp);
+        }
+
+        // Y Rotation (Yaw Rotation)
+        //Quaternion TargetLocalRotation = Quaternion.Euler(0.0f, character.characterRotation.y, 0.0f);
+
+        // X Rotation (Pitch Rotation)
+        pivotTargetLocalRotation = Quaternion.Euler(character.characterRotation.x, character.characterRotation.y, 0.0f);
+
+        if (RotationSpeed > 0.0f)
+        {
+            //transform.localRotation = Quaternion.Slerp(transform.localRotation, TargetLocalRotation, RotationSpeed * Time.deltaTime);
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, pivotTargetLocalRotation, RotationSpeed * Time.deltaTime);
+        }
+        else
+        {
+            //transform.localRotation = TargetLocalRotation;
+            transform.localRotation = pivotTargetLocalRotation;
+        }
+    }
+
+    private void UpdateControllerRotation()
+    {
+        // Adjust the pitch angle (X Rotation)
+        float pitchAngle = character.characterRotation.x;
+        pitchAngle -= CameraInput.y * ControlRotationSensitivity;
+
+        // Adjust the yaw angle (Y Rotation)
+        float yawAngle = character.characterRotation.y;
+        yawAngle += CameraInput.x * ControlRotationSensitivity;
+
+        character.characterRotation = new Vector2(pitchAngle, yawAngle); 
+    }
+
+    void CalculateRotation(Vector2 RawInput)
+    {
+        //Quaternion yawRotation = Quaternion.Euler(0.0f, controlRotation.y, 0.0f);
+
     }
 }
---
+++ Assets/Script/Character.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class Character : MonoBehaviour
+{
+    public float MaxSpeed = 8.0f;
+
+    public float _targetHorizontalSpeed; // In meters/second
+    public float _horizontalSpeed; // In meters/second
+
+    // TODO: Move to private
+    public Animator animator;
+    public CharacterController characterController;
+
+    public Vector2 characterRotation; // X (Pitch), Y (Yaw)
+    public Vector3 OSmovementInput;
+    public Vector3 LastInput;
+    public Vector2 CameraInput;
+    public Vector2 RawMoveInput;
+    public bool HasMovementInput;
+
+    public Vector2 controlRotation;
+
+    public Vector3 Velocity => characterController.velocity;
+    public Vector3 HorizontalVelocity => new Vector3(characterController.velocity.x, 0.0f, characterController.velocity.z);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Velocity = characterController.velocity;
+        //HorizontalVelocity = new Vector3(characterController.velocity.x, 0.0f, characterController.velocity.z);
+        HasMovementInput = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputTick();
+        UpdateAnimation();
+        Debug.Log(HorizontalVelocity);
+    }
+
+    private void FixedUpdate()
+    {
+        UpdateSpeed();
+        Movement();
+    }
+
+    public void InputTick()
+    {
+        RawMoveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        Quaternion yawRotation = Quaternion.Euler(0.0f, characterRotation.y, 0.0f);
+        Vector3 forward = yawRotation * Vector3.forward;
+        Vector3 right = yawRotation * Vector3.right;
+        Vector3 NewmovementInput = (forward * RawMoveInput.y + right * RawMoveInput.x);
+
+        if (NewmovementInput.sqrMagnitude > 1f)
+        {
+            NewmovementInput.Normalize();
+        }
+
+        // TODO: AxisDeadZone
+        bool CurrentMoveInput = NewmovementInput.sqrMagnitude > 0.0f;
+        if (HasMovementInput && !CurrentMoveInput)
+        {
+            LastInput = OSmovementInput;
+        }
+
+        OSmovementInput = NewmovementInput;
+        HasMovementInput = CurrentMoveInput;
+    }
+
+    void UpdateAnimation()
+    {
+        float normHorizontalSpeed = HorizontalVelocity.magnitude / MaxSpeed;
+        animator.SetFloat("HorizontalSpeed", normHorizontalSpeed);
+    }
+
+    public void UpdateSpeed()
+    {
+        Debug.Log(OSmovementInput);
+        Vector3 movementInput = OSmovementInput;
+        if (movementInput.sqrMagnitude > 1.0f)
+        {
+            movementInput.Normalize();
+        }
+
+        _targetHorizontalSpeed = movementInput.magnitude * MaxSpeed;
+        float acceleration = HasMovementInput ? 25f : 25f;
+
+        _horizontalSpeed = Mathf.MoveTowards(_horizontalSpeed, _targetHorizontalSpeed, acceleration * Time.deltaTime);
+    }
+
+
+    public void Movement()
+    {
+        Vector3 movement = _horizontalSpeed * GetMovementDirection();
+        characterController.Move(movement * Time.deltaTime);
+        Vector3 HorizontalMovement = new Vector3(movement.x, 0.0f, movement.z);
+        // Rotate
+        OrientToTargetRotation(HorizontalMovement);
+    }
+
+    public void SetControlRotation(Vector2 controlRotation)
+    {
+        // Adjust the pitch angle (X Rotation)
+        float pitchAngle = controlRotation.x;
+        pitchAngle %= 360.0f;
+        pitchAngle = Mathf.Clamp(pitchAngle, -45f, 75f);
+
+        // Adjust the yaw angle (Y Rotation)
+        float yawAngle = controlRotation.y;
+        yawAngle %= 360.0f;
+
+        controlRotation = new Vector2(pitchAngle, yawAngle);
+    }
+
+    void OrientToTargetRotation(Vector3 horizontalMovement)
+    {
+        if (horizontalMovement.sqrMagnitude > 0.0f)
+        {
+            float rotationSpeed = Mathf.Lerp(1200f, 600f, _horizontalSpeed / _targetHorizontalSpeed);
+
+            Quaternion targetRotation = Quaternion.LookRotation(horizontalMovement, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
+    }
+
+    private Vector3 GetMovementDirection()
+    {
+        Vector3 moveDir = HasMovementInput ? OSmovementInput : LastInput ;
+        if (moveDir.sqrMagnitude > 1f)
+        {
+            moveDir.Normalize();
+        }
+
+        return moveDir;
+    }
+}
